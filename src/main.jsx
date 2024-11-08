2501156448fe63ec7774030c2b3f37c4187ee89a
GhostDevil274 <minhanhnguyentran274@gmail.com>
2024-11-08 18:48:03 +0700

    Làm gọn code lại

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <AdminViewReport />,
+        element: <OfficerReport />,
       },
       {
         path: "/upload",
--- src/pages/AdminHomepage.jsx
+++ src/pages/AdminHomepage.jsx
@@ -1,14 +1,32 @@
 import "../App.css";
-
-import AdminOverview from "../components/Admin/AdminOverview";
-
+import AdminAvatar from "../components/Admin/AdminOverview/AdminAvatar";
+import AdminViewReportButton from "../components/Admin/AdminOverview/AdminViewReportButton";
+import Options from "../components/Admin/AdminOverview/Options";
 
 function AdminHomepage() {
   return (
-    <>
-      <AdminOverview />
-    </>
+    <div className="mt-[80px] flex justify-center items-center">
+      <div className="w-[1454px] bg-thirdary p-[40px] rounded-[25px] flex justify-between gap-[100px] mb-[120px]">
+        <div className="font-light flex flex-row items-center ml-[100px] gap-[20px]">
+            <div className="flex flex-col items-center">
+                <AdminAvatar
+                    imageUrl={"/user-logo-big.svg"}
+                    adminName={"Nguyễn Tuấn Anh"}
+                    adminID={"2252038"}
+                />
+                <div className="mt-[20px] bg-[#A68BC1] text-xl rounded-[25px] mt-[5px] w-[245px] flex items-center justify-center h-[52px]">
+                    <AdminViewReportButton />
+                </div>
+            </div>
+
+            <div>
+                <Options />
+            </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default AdminHomepage;
+
